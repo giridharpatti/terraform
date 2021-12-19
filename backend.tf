0f5a38f2ae887e39e68ab72e6a611149893edcde
giridharpatti <86153500+giridharpatti@users.noreply.github.com>
2021-12-19 14:13:37 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
  backend "s3" {
    bucket         = "giriterraform"
-   key            = "/terraform.tfstate"
+   key            = "terraform.tfstate"
    region         = "var.region"
  }
 }
