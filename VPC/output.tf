05caf08e030600759eeb023b80b11b5287f70f24
giridharpatti <86153500+giridharpatti@users.noreply.github.com>
2021-12-19 13:45:16 +0530

    Delete VPC directory

--- VPC/output.tf
+++
@@ -1,8 +0,0 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = aws_vpc.shoppingcart-test.id
-}
-output "internet_gateway" {
-  description = "The ID of the igw"
-  value       = aws_internet_gateway.gw.id
-}
--- VPC/variable.tf
+++
@@ -1,7 +0,0 @@
-variable "envname"{}
-variable "cidr"{}
-variable "azs"{}
-variable "pubsubnets"{}
-variable "privatesubnets"{}
-variable "datasubnets"{}
-variable "region" {}
